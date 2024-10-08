39cf70df99a881bfbba7e91d648e51e876fdc8de
Yuvika Dube <yuvikadube@gmail.com>
2024-10-08 15:49:09 -0700

    open book and print word count

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+def main():
+    bookPath = "books/frankenstein.txt"
+    contents = getBookText(bookPath)
+    count = getWordCount(contents)
+    print(count)
+    
+def getWordCount(contents):
+    count = 0
+    words = contents.split()
+    for word in words:
+        count += 1
+    return count
+
+
+def getBookText(path):
+    with open("books/frankenstein.txt") as book:
+        contents = book.read()
+        book.close()
+        return contents
+    
+main()
