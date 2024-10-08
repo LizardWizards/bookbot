9d5a6f82b090ca08bb3d6f352d70ee2e0b23f9ae
Yuvika Dube <yuvikadube@gmail.com>
2024-10-08 16:05:50 -0700

    print book report

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 def main():
     bookPath = "books/frankenstein.txt"
     contents = getBookText(bookPath)
-    count = getWordCount(contents)
-    print(count)
+    wordCount = getWordCount(contents)
+    charCounts = getCharCounts(contents)
+
+    print(f"{wordCount} words found in the document")
+    print("")
+
+    for charCount in charCounts:
+        if charCount.isalpha():
+            count = charCounts[charCount]
+            print(f"The '{charCount}' was found {count} times")
     
 def getWordCount(contents):
     count = 0
@@ -11,11 +19,20 @@ def getWordCount(contents):
         count += 1
     return count
 
-
 def getBookText(path):
     with open("books/frankenstein.txt") as book:
         contents = book.read()
         book.close()
         return contents
     
+def getCharCounts(text):
+    counts = {}
+    for char in text:
+        char = char.lower()
+        if not char in counts:
+            counts[char] = 1
+        else:
+            counts[char] += 1
+    return counts
+
 main()
